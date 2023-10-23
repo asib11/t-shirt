d89b3bf247d12e02bac8cb4520b65ba15cb17e0a
asib <ahmedasib1@gmail.com>
2023-10-23 18:35:53 +0600

    condition rander and css

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,14 +2,24 @@ import './Cart.css'
 import PropTypes from 'prop-types'
 
 const Cart = ({cart,removeCartHandler}) => {
+    let message;
+    if(cart.length === 0){
+        message = <p>The cart is epmty</p>
+    }else{
+        message = <p>add {cart.length} products</p>
+    }
     return (
         <div className='cart' >
             <h2>cart: {cart.length} </h2>
+            {message}
             {
                 cart.map(tShirt =><p className="cart-style"
                     key={tShirt._id}
                 >{tShirt.name} <button 
                 onClick={() => removeCartHandler(tShirt._id)}>X</button> </p>)
+            }
+            {
+                cart.length >=5 && <p className={`common ${cart.length >5 ? 'green': 'more-green'}`}>you have goten some discount</p>
             }
         </div>
     );
