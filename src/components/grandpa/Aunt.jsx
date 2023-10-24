80b80b7a2ce0d047b6dd3a433b68a4ee122e7607
asib <ahmedasib1@gmail.com>
2023-10-24 17:21:38 +0600

    context api itrrigrating

--- src/components/grandpa/Aunt.jsx
+++ src/components/grandpa/Aunt.jsx
@@ -1,9 +1,13 @@
+import { useContext } from "react";
 import Cousin from "./Cousin";
+import { MoneyContext } from "./Grandpa";
 
 const Aunt = () => {
+    const [money] = useContext(MoneyContext);
     return (
         <div>
             <p>Aunt</p>
+            <p><small>grandpa money: {money}</small></p>
             <section className="flex">
                 <Cousin>cousin-1</Cousin>
                 <Cousin>cousin-2</Cousin>
